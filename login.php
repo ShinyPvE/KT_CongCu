608cc825df3744141790fa0242ecc776c89ad51c
minhnhat <nnht133@gmail.com>
2023-03-01 10:40:29 +0700

    add login

--- login.php
+++ login.php
@@ -13,8 +13,9 @@
     <form method="post" action="login.php">
         <label>Ten Dang Nhap :</label>
         <input type="text" name="username" required><br><br>
-        <label>Mat Khau:</label>
+        <label>Password</label>
         <input type="password" name="password" required><br><br>
+        <input type="submit" name="login" value="Login">
     </form>
 
 </body>
